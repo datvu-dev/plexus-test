89fa28356fec2002845359abfe81d99ac07f32ce
Dat Vu <datvu@ekit-inc.com>
2020-03-01 21:03:41 +1100

    Set up routing for the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import {Route, Link, BrowserRouter as Router} from "react-router-dom";
+import MainForm from "./components/MainForm";
+import SecondaryForm from "./components/SecondaryForm";
  
 class App extends Component {
   render() {
     return (
         <div>
-          
+          <Router>
+            <div>
+              <Link to="/">home</Link>
+              <Link to="/next">next</Link>
+            </div>
+            <div className="content">
+              <Route exact path="/" component={MainForm}/>
+              <Route path="/next" component={SecondaryForm} />
+            </div>
+          </Router>
         </div>
     );
   }
