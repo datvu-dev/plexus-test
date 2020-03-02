d58cc64dfb14347d8a3f5a1fc78c62a416f56c66
Dat Vu <datvu@ekit-inc.com>
2020-03-02 16:33:06 +1100

    Set up initial state for the app and add function to handle input change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,31 @@ import MainForm from "./components/MainForm";
 import SecondaryForm from "./components/SecondaryForm";
  
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            storeType: '',
+            storeDetails: '',
+            firstName: '',
+            lastName: '',
+            userRole: '',
+            joinDate: null,
+
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+      const target = event.target;
+      const value = target.type === 'checkbox' ? target.checked : target.value;
+      const name = target.name;
+  
+      this.setState({
+        [name]: value
+      });
+    }
+
   render() {
     return (
         <div>
@@ -13,7 +38,7 @@ class App extends Component {
               <Link to="/next">next</Link>
             </div>
             <div className="content">
-              <Route exact path="/" component={MainForm}/>
+              <Route exact path="/" render={() => <MainForm state={this.state} handleInputChange={this.handleInputChange} />} />
               <Route path="/next" component={SecondaryForm} />
             </div>
           </Router>
