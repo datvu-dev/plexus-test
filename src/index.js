9004378d3c89bca7b597525a6c018fbe75e118ee
Dat Vu <datvu@ekit-inc.com>
2020-03-03 11:22:23 +1100

    Wrap app in toast notification provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ToastProvider } from 'react-toast-notifications'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <ToastProvider>
+        <App/>
+    </ToastProvider>
+, document.getElementById('root'));
